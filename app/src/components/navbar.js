f2fdcf7fcf5fe6314886f26be0321c62736977d5
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-04-10 00:27:52 -0400

    Add basic table view

---
+++ app/src/components/navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+ 
+import "bootstrap/dist/css/bootstrap.css";
+ 
+import { NavLink } from "react-router-dom";
+ 
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <h2>SportsCardTool</h2>
+       <NavLink className="navbar-brand" to="/">
+       </NavLink>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+
+     </nav>
+   </div>
+ );
+}
---
+++ app/src/components/recordList.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import MaterialReactTable from 'material-react-table';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+ 
+export default function RecordList() {
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefetching, setIsRefetching] = useState(false);
+  const [rowCount, setRowCount] = useState(0);
+
+
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 20,
+  });
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!data.length) {
+        setIsLoading(true);
+      } else {
+        setIsRefetching(true);
+      }
+
+      const url = new URL(
+        '/api/v1/sportscards/search',
+        process.env.NODE_ENV === 'production'
+          ? 'http://travisapi.pythonanywhere.com'
+          : 'http://localhost:5000',
+      );
+
+      columnFilters.forEach((filter, _i) => url.searchParams.set(filter['id'], filter['value']));
+      url.searchParams.set("page", pagination.pageIndex)
+
+      try {
+        const response = await fetch(url.href);
+        const json = await response.json();
+        const cards = json['cards']
+        setData(cards);
+        setRowCount(json['total_results']);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        return;
+      }
+      setIsError(false);
+      setIsLoading(false);
+      setIsRefetching(false);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    columnFilters,
+    pagination.pageIndex,
+    pagination.pageSize,
+    sorting,
+  ]);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'front_img',
+        header: 'Image',
+        Cell: ({ cell }) => <img alt="Card Front" src={cell.getValue()} />,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        size: 60
+      },
+      {
+        accessorKey: 'team',
+        header: 'Team',
+        size: 60
+      },
+      {
+        accessorKey: 'listing',
+        header: 'Listing',
+      },
+      {
+        accessorKey: 'serial',
+        header: "Numbered",
+        Cell: ({ cell }) => {
+        const cellValue = cell.getValue()
+        if (cellValue === 0) {
+          return "False"
+        } else {
+          return cellValue
+        }
+        },
+        size: 60,
+      },
+      {
+        accessorKey: '_id',
+        header: 'Edit',
+        Cell: ({ cell }) => {
+          return <Button onClick={()=> navigate('edit/'+cell.getValue())} variant="contained">Edit</Button>
+        },
+        size: 20,
+        enableColumnFilter: false,
+        enableSorting: false
+      }
+      
+    ],
+    [navigate],
+  );
+
+  return (
+   <div>
+     <h3>Record List</h3>
+     <MaterialReactTable
+      columns={columns}
+      data={data}
+      getRowId={(row) => row.phoneNumber}
+      initialState={{ showColumnFilters: true }}
+      manualFiltering
+      manualPagination
+      manualSorting
+      muiToolbarAlertBannerProps={
+        isError
+          ? {
+              color: 'error',
+              children: 'Error loading data',
+            }
+          : undefined
+      }
+      onColumnFiltersChange={setColumnFilters}
+      onPaginationChange={setPagination}
+      muiTableProps={{
+        sx: {
+          tableLayout: 'fixed',
+        },
+      }}
+      muiTablePaginationProps={{
+        rowsPerPageOptions: [],
+        showFirstButton: false,
+        showLastButton: false,
+      }}
+      onSortingChange={setSorting}
+      rowCount={rowCount}
+      state={{
+        columnFilters,
+        isLoading,
+        pagination,
+        showAlertBanner: isError,
+        showProgressBars: isRefetching,
+        sorting,
+      }}
+    />
+   </div>
+ );
+}
