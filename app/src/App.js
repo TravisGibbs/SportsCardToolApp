989f010cf5b80542bc43cc85b88413199801361a
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-04-11 13:42:52 -0400

    Style Scrollbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
  
 import { Route, Routes } from "react-router-dom";
- 
+import GlobalStyles from '@mui/material/GlobalStyles';
+
 import Navbar from "./components/navbar";
 import RecordList from "./components/recordList";
 import Edit from "./components/edit";
@@ -9,6 +10,20 @@ import Edit from "./components/edit";
 const App = () => {
  return (
    <div>
+     <GlobalStyles
+        styles={{
+          '*::-webkit-scrollbar': {
+            width: '0.4em',
+          },
+          '*::-webkit-scrollbar-track': {
+            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+          },
+          '*::-webkit-scrollbar-thumb': {
+            backgroundColor: 'rgba(0,0,0,.1)',
+            outline: '1px solid slategrey',
+          },
+        }}
+      />
      <Navbar />
      <Routes>
        <Route exact path="/" element={<RecordList />} />
