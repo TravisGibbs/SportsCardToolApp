78be80d776c15b057d0d28e3b68affa11faa31e7
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-04-23 11:07:54 -0400

    Move sales to new component

--- app/src/components/view.js
+++ app/src/components/view.js
@@ -16,9 +16,7 @@ import {makeStyles} from '@material-ui/core';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import MaterialReactTable from 'material-react-table';
-import LinearProgress from '@mui/material/LinearProgress';
-
-const parse = require('html-react-parser');
+import Sales from './sales';
 
 const placeholder_url = require('../assets/baseball-card.png');
 
@@ -62,7 +60,6 @@ export default function View() {
   const [snackSeverity, setSnackSeverity] = React.useState('');
   const [formOpen, setFormOpen] = React.useState(false);
   const [currentEbayLink, setCurrentEbayLink] = React.useState('');
-  const [currentPoint, setCurrentPoint] = React.useState(null);
   const [pagination, setPagination] = useState({
     pageIndex: 0,
     pageSize: 20,
@@ -196,61 +193,6 @@ export default function View() {
 
     handleClose();
   };
-
-  useEffect(() => {
-    async function fetchPoint() {
-      if (form.names.length > 0) {
-        const searchTerm = (
-          form.set_alt.trim() +
-          '%2B' +
-          form.names.join('%2B')
-        ).replace(/ /g, '%2B');
-        const response = await fetch('https://back.130point.com/sales/', {
-          headers: {
-            accept: '*/*',
-            'accept-language': 'en-US,en;q=0.9',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'sec-ch-ua':
-              '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-          },
-          referrer: 'https://130point.com/',
-          referrerPolicy: 'strict-origin-when-cross-origin',
-          body: 'query=' + searchTerm + '&type=2&subcat=-1',
-          method: 'POST',
-          mode: 'cors',
-          credentials: 'omit',
-        });
-
-        if (!response.ok) {
-          const message = `An error has occurred: ${response.statusText}`;
-          window.alert(message);
-          return;
-        }
-
-        let data = await response.text();
-
-        const entries = data.split('var offerData =').splice(1);
-
-        entries.forEach(element => {
-          // TODO: Finding best offers requires pinging another access point indvidually refrain from doing so for now?
-          // const sale_data = JSON.parse(element.split(";")[0])
-          // data = data.replace("<b>Best Offer Sale - Loading Actual Sold Price</b>", '<b>Sale Price:</b> <span class="bidLink" style="color: green; font-weight: bold; text-decoration-line: underline; background-color: lightgreen;">'+sale_data["2"]+sale_data["3"]+'</span>')
-          data = data.replace(' - Loading Actual Sold Price', '');
-        });
-
-        setCurrentPoint(parse(data));
-      }
-    }
-
-    fetchPoint();
-
-    return;
-  }, [form.names, form.set_alt]);
 
   useEffect(() => {
     async function fetchCurrentEntry() {
@@ -522,13 +464,7 @@ export default function View() {
             <Card>
               <h1 style={{marginLeft: 10}}>Recent {form.names} Sales</h1>
 
-              {currentPoint ? (
-                currentPoint
-              ) : (
-                <Box sx={{width: '100%'}}>
-                  <LinearProgress />
-                </Box>
-              )}
+              <Sales names={form.names} set_alt={form.set_alt} />
             </Card>
           </Grid>
           <Grid xs={8}>
