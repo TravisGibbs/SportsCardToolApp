5525d96921b6a3959409f450a1bba46b74ab63b8
Travis Gibbs <travisgibbs.2019@gmail.com>
2023-05-30 21:34:04 -0400

    Add Button and Tweak CSS

--- app/src/components/home.js
+++ app/src/components/home.js
@@ -1,9 +1,13 @@
 import {
+  Button,
     makeStyles,
     Typography
   } from '@material-ui/core';
+import Grid from '@mui/material/Grid';
+import {useNavigate} from 'react-router-dom';
 
-const useStyles = makeStyles(theme => ({
+
+const useStyles = makeStyles(() => ({
     backgroundImg: {
         margin: '1%',
         backgroundBlendMode: "saturation",
@@ -15,29 +19,58 @@ const useStyles = makeStyles(theme => ({
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover"
     },
-    fullHeight: {
-      height: "100%"
+    pageHeight: {
+      height: "80%"
     },
     menu: {
       top: "20%",
-      left: "10%",
+      width: "70%",
+      marginLeft: "10%",
       position: "absolute"
     },
     largeText: {
-      fontSize: "5vw",
+      fontSize: "8vw",
       color: "#7cb69d"
+    },
+    mediumText: {
+      fontSize: "2.5vw",
+      color: "#7cb69d",
+      marginLeft: "5%"
+    },
+    smallText: {
+      fontSize: "1vw",
+      color:"#FFFFFF"
+    },
+    buttonBackground: {
+      backgroundColor: "#FFFFFF !important"
     }
   }));
 
 export default function Home() {
     const classes = useStyles();
+    const navigate = useNavigate();
 
     return (
-      <div className={classes.fullHeight}>
+      <div className={classes.pageHeight}>
         <div className={classes.backgroundImg}>
         </div>
         <div className={classes.menu}>
           <Typography className={classes.largeText}>SportsCardTool</Typography>
+          <Typography className={classes.mediumText}>The Future of the Hobby Starts Here</Typography>
+          <br/>
+          <br/>
+          <Grid container rowSpacing={5} spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Button onClick={() => navigate('cards/')} className={classes.buttonBackground} variant="contained">
+                Explore Card Database
+              </Button>
+            </Grid>            
+            <Grid item xs={12} sm={6}>
+              <Button className={classes.buttonBackground} variant="contained" disabled={true}>
+                Explore Release Database (coming soon)
+              </Button>
+            </Grid>
+          </Grid>
         </div>
       </div>
     )
